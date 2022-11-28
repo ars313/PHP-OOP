cc196871f886ecf3b1f11fd09dbe6914255c1255
Nurkholis <m_nurkholis@icloud.com>
2022-11-28 14:18:04 +0700

    first commit

--- test.php
+++ test.php
@@ -1,2 +1,18 @@
 <?php
-echo "Hello World!";
+
+#prime number
+class Product {
+    public $type;
+    public $brand;
+
+    function productChoise() {
+        return $this->type. "Brand produk adalah ...". $this->brand;
+    }
+}
+
+$product1 = new Product();
+
+$product1->type="Kulkas";
+$product1->brand="LG";
+
+echo $product1->productChoise();
