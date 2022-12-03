999c475d382cfeca99c5954026a7b59064174ead
Nurkholis <m_nurkholis@icloud.com>
2022-12-03 10:18:02 +0700

    static

---
+++ 65.static_method_increment.php
@@ -0,0 +1,12 @@
+<?php
+//class Product {
+//    private static $products = 0 ;
+//    public function __construct() {
+//        self::$products++;
+//        echo "Create class Product, total Product = " . self::$products . "<br />";
+//    }
+//}
+//
+//$productA = new Product();
+//$productB = new Product();
+//$productC = new Product();
