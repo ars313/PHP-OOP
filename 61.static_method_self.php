8dab2ef2263e2aa38250f88c70a2bd23a1c92874
Nurkholis <m_nurkholis@icloud.com>
2022-12-03 09:43:06 +0700

    static

---
+++ 61.static_method_self.php
@@ -0,0 +1,15 @@
+<?php
+//
+//class Product {
+//    public static $stock = 100;
+//
+//    public static function getstock() {
+//        return "Stock product available for purchase order ...". self::$stock;
+//    }
+//}
+//
+//class Blender extends Product {
+//
+//}
+//
+//echo Blender::getstock();
