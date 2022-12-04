2c3579b00a4d281747f43121e799c92808dbfc7e
Nurkholis <m_nurkholis@icloud.com>
2022-12-04 14:57:05 +0700

    static

--- vendor/class_constant_err.php
+++ vendor/class_constant_err.php
@@ -1,11 +1,11 @@
 <?php
-class Product {
-    public static $kursSGD = 10000;
-    public const kursUSD = 14000;
-}
-
-Product::$kursSGD = 12000;
-echo Product::$kursSGD . "<br/>";
-
-
-echo Product::kursUSD;
+//class Product {
+//    public static $kursSGD = 10000;
+//    public const kursUSD = 14000;
+//}
+//
+//Product::$kursSGD = 12000;
+//echo Product::$kursSGD . "<br/>";
+//
+//
+//echo Product::kursUSD;
