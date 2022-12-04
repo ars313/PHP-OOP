a63632f5fda06bfb4515bd5ec4e9676dd337cdc9
Nurkholis <m_nurkholis@icloud.com>
2022-12-04 15:05:04 +0700

    static

---
+++ 72.constructor_promotion_property_1.php
@@ -0,0 +1,21 @@
+<?php
+//class Product {
+//    public function __construct(
+//        public $type,
+//        public $brand,
+//        public $stock) {
+//    }
+//}
+//
+//
+//$product = new Product("Televisi", "Samsung", 60);
+//$product01 = new Product("Washing Machine", "LG", 70);
+//
+//echo "<pre>";
+//print_r($product01);
+//echo "</pre>";
+//
+//echo "<pre>";
+//print_r($product01);
+//echo "</pre>";
+
