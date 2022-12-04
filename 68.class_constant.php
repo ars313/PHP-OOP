0fcde9d888a80975d5a2f4d247f94e2e037e7076
Nurkholis <m_nurkholis@icloud.com>
2022-12-04 14:50:54 +0700

    static

--- 68.class_constant.php
+++ 68.class_constant.php
@@ -1,13 +1,13 @@
 <?php
-class Product {
-    public $priceUSD = 0;
-    private const KURSUSD = 15000;
-
-    public function priceIDR() {
-        return $this->priceUSD * self::KURSUSD;
-    }
-}
-
-$convertIDR = new Product;
-$convertIDR->priceUSD = 15;
-echo $convertIDR->priceIDR();
+//class Product {
+//    public $priceUSD = 0;
+//    private const KURSUSD = 15000;
+//
+//    public function priceIDR() {
+//        return $this->priceUSD * self::KURSUSD;
+//    }
+//}
+//
+//$convertIDR = new Product;
+//$convertIDR->priceUSD = 15;
+//echo $convertIDR->priceIDR();
