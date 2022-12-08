2a3e646b3115451d1259326490a6f8b3eab3c5bf
Nurkholis <m_nurkholis@icloud.com>
2022-12-08 19:19:28 +0700

    static

---
+++ AbstractClass/07.abstract_class_property_method.php
@@ -0,0 +1,19 @@
+<?php
+abstract class Product {
+    private $stock = 100;
+    abstract public function checkPrice();
+
+    public function getStock() {
+        return $this->stock;
+    }
+}
+
+class Televisi extends Product {
+    public function checkPrice() {
+        return 30000;
+    }
+}
+
+$product1 = new Televisi();
+echo $product1->checkPrice() . "<br>";
+echo $product1->getStock() . "<br>";
