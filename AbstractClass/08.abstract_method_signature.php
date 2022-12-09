9f81695600959eb711b4bc1418bef7f12b077bb3
Nurkholis <m_nurkholis@icloud.com>
2022-12-09 11:45:24 +0700

    static

---
+++ AbstractClass/08.abstract_method_signature.php
@@ -0,0 +1,15 @@
+<?php
+////abstract class Product
+//abstract class Product {
+//    abstract public function checkPrice($total);
+//}
+//
+//class Televisi extends Product {
+//    public function checkPrice($total)
+//    {
+//        return 3000000 * $total;// TODO: Implement checkPrice() method.
+//    }
+//}
+//
+//$product01 = new Televisi();
+//echo $product01->checkPrice(40);
