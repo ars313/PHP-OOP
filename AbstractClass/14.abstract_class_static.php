554575bb9b5bed8c8cfe1e4b33b2dddf1a63fd0c
Nurkholis <m_nurkholis@icloud.com>
2022-12-09 12:14:48 +0700

    abstract class

--- AbstractClass/14.abstract_class_static.php
+++ AbstractClass/14.abstract_class_static.php
@@ -1,12 +1,12 @@
 <?php
-abstract class Product {
-    public static $totalProduct = 100;
-    public static function checkProduct() {
-        return "Total Product: " . self::$totalProduct;
-    }
-}
-
-echo "********************************<br>";
-echo "Product: ". Product::checkProduct();
-echo "********************************<br>";
-echo  Product::$totalProduct;
+//abstract class Product {
+//    public static $totalProduct = 100;
+//    public static function checkProduct() {
+//        return "Total Product: " . self::$totalProduct;
+//    }
+//}
+//
+//echo "********************************<br>";
+//echo "Product: ". Product::checkProduct();
+//echo "********************************<br>";
+//echo  Product::$totalProduct;
