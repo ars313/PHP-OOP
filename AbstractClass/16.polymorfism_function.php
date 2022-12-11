8eb532b92425f52546a8e5a00c644a2c67b494d2
Nurkholis <m_nurkholis@icloud.com>
2022-12-11 13:10:03 +0700

    abstract class

--- AbstractClass/16.polymorfism_function.php
+++ AbstractClass/16.polymorfism_function.php
@@ -1,39 +1,39 @@
 <?php
-abstract class Product {
-    abstract public function cekBrand();
-}
-
-class Televisi extends Product {
-    public function cekBrand()
-    {
-        return "Polytron";
-    }
-}
-
-class WashingMachine extends Product {
-    public function cekBrand()
-    {
-        return "Electrolux";
-    }
-}
-
-class IceMachine extends Product {
-    public function cekBrand()
-    {
-        return "Diamonds";
-    }
-}
-
-$product01 = new Televisi();
-$product02 = new WashingMachine();
-$product03 = new IceMachine();
-
-function showBrand($objectProduct) {
-    return $objectProduct->cekBrand() . "<br>";
-}
-
-echo showBrand($product01);
-echo showBrand($product02);
-echo showBrand($product03);
-
-?>
+//abstract class Product {
+//    abstract public function cekBrand();
+//}
+//
+//class Televisi extends Product {
+//    public function cekBrand()
+//    {
+//        return "Polytron";
+//    }
+//}
+//
+//class WashingMachine extends Product {
+//    public function cekBrand()
+//    {
+//        return "Electrolux";
+//    }
+//}
+//
+//class IceMachine extends Product {
+//    public function cekBrand()
+//    {
+//        return "Diamonds";
+//    }
+//}
+//
+//$product01 = new Televisi();
+//$product02 = new WashingMachine();
+//$product03 = new IceMachine();
+//
+//function showBrand($objectProduct) {
+//    return $objectProduct->cekBrand() . "<br>";
+//}
+//
+//echo showBrand($product01);
+//echo showBrand($product02);
+//echo showBrand($product03);
+//
+//?>
