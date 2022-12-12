e39618db0c1ce10086eee80c513a2d2526b88ff3
Nurkholis <m_nurkholis@icloud.com>
2022-12-12 19:08:28 +0700

    abstract class

--- Interfaces/7.interfaces_basic.php
+++ Interfaces/7.interfaces_basic.php
@@ -1,5 +1,21 @@
 <?php
-//interface ProductExport {
-//    public function CheckPriceUSD();
-//    public function CkeckCountry();
-//}
+
+interface ProductExport {
+    public function CheckPriceUSD();
+    public function CheckCountry();
+}
+
+class Televisi implements ProductExport {
+    public function CheckPriceUSD() {
+        return 185;
+    }
+
+    public function CheckCountry() {
+        return ["Singapore", "Malaysia", "Thailand"];
+    }
+}
+
+$product = new Televisi();
+echo $product->CheckPriceUSD();
+echo "<br/>";
+echo implode(", ", $product->CheckCountry());
