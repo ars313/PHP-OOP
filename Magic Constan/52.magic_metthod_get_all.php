66d63a34e8ca3009edb74ef5cfd79a84705ea479
Nurkholis <m_nurkholis@icloud.com>
2022-12-13 21:37:59 +0700

    magic method get

---
+++ Magic Constan/52.magic_metthod_get_all.php
@@ -0,0 +1,24 @@
+<?php
+//class Product {
+//    private $brand = "Logitech";
+//    private $type = "Keyboard";
+//
+//    public function __get($name) {
+//        if (($name == "brand") || ($name == "type"))
+//        {
+//            $hasil = strtoupper($this->$name);
+//        }
+//
+//        else {
+//            $hasil = "chek pointing";
+//        }
+//        return $hasil;
+//    }
+//}
+
+$product = new Product();
+echo $product->brand;
+echo "<br>";
+echo $product->type;
+echo "<br>";
+echo $product->warna;
