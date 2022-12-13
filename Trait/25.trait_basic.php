2ee6641bf9d3d581188ee38e9ad1af9eb9307a48
Nurkholis <m_nurkholis@icloud.com>
2022-12-13 13:00:21 +0700

    abstract class

---
+++ Trait/25.trait_basic.php
@@ -0,0 +1,28 @@
+<?php
+//
+//class Television {
+//    public function checkResolution()
+//    {
+//        return "Full Resolutions";
+//    }
+//}
+//
+//trait SmartElectronic {
+//    public function checkOS()
+//    {
+//        return "Android 9.0 (Pie)";
+//    }
+//}
+//
+//class SmartTv extends Television {
+//    use SmartElectronic;
+//
+//    public function checkInfo()
+//    {
+//        return "Smart TV" . $this->checkResolution() . "-" . $this->checkOS();
+//    }
+//}
+//
+//
+//$product = new SmartTv();
+//echo $product->checkInfo();
