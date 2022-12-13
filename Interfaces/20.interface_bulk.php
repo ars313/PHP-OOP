73667e3f9703e925eb32def0148d01526266f05c
Nurkholis <m_nurkholis@icloud.com>
2022-12-13 12:50:55 +0700

    abstract class

---
+++ Interfaces/20.interface_bulk.php
@@ -0,0 +1,44 @@
+<?php
+//interface ExportProduct {
+//    public function checkPriceUSD();
+//    public function  checkCountry();
+//}
+//
+//interface FoodProduct {
+//    public function checkExpired();
+//}
+//
+//interface FoodFrozenProduct {
+//    public function checkTemperatureMin();
+//}
+//
+//class Nugget implements ExportProduct, FoodProduct, FoodFrozenProduct {
+//    public function CheckPriceUSD()
+//    {
+//        return 15000;
+//    }
+//
+//    public function CheckCountry()
+//    {
+//        return ["Malaysia", "Canada", "Indonesia"];
+//    }
+//
+//    public function checkExpired()
+//    {
+//        return "February 2023";
+//    }
+//
+//    public function checkTemperatureMin()
+//    {
+//        return -14;
+//    }
+//}
+//
+//$product = new Nugget();
+//echo $product->CheckPriceUSD();
+//echo "<br/>";
+//echo implode(", ", $product->CheckCountry());
+//echo "<br/>";
+//echo $product->checkExpired();
+//echo "<br/>";
+//echo $product->checkTemperatureMin();
