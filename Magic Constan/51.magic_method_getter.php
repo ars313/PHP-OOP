4cec3c59af353f18cc832a3a37e4db73630b546a
Nurkholis <m_nurkholis@icloud.com>
2022-12-13 21:24:22 +0700

    magic method get

--- Magic Constan/51.magic_method_getter.php
+++ Magic Constan/51.magic_method_getter.php
@@ -1,37 +1,37 @@
 <?php
-class Product {
-    private $brand = "Logitech";
-    private $price = 1500000;
-    private $type = "Keyboard";
-
-    public function __get($name)
-    {
-        if ($name == "brand")
-        {
-            $hasil = strtoupper($this->brand);
-        }
-
-        else if ($name == "price")
-        {
-            $hasil = "Rp. ".number_format($this->price, 2, ",",".");
-        }
-
-        else if ($name == "type")
-        {
-            $hasil = "Type product is : ". $this->type;
-        }
-        else {
-            $hasil = "Product uknown $name already";
-        }
-        return $hasil;
-    }
-}
-
-$product = new Product();
-echo $product->brand;
-echo "<br>";
-echo $product->price;
-echo "<br>";
-echo $product->type;
-echo "<br>";
-echo $product->sport;
+//class Product {
+//    private $brand = "Logitech";
+//    private $price = 1500000;
+//    private $type = "Keyboard";
+//
+//    public function __get($name)
+//    {
+//        if ($name == "brand")
+//        {
+//            $hasil = strtoupper($this->brand);
+//        }
+//
+//        else if ($name == "price")
+//        {
+//            $hasil = "Rp. ".number_format($this->price, 2, ",",".");
+//        }
+//
+//        else if ($name == "type")
+//        {
+//            $hasil = "Type product is : ". $this->type;
+//        }
+//        else {
+//            $hasil = "Product uknown $name already";
+//        }
+//        return $hasil;
+//    }
+//}
+//
+//$product = new Product();
+//echo $product->brand;
+//echo "<br>";
+//echo $product->price;
+//echo "<br>";
+//echo $product->type;
+//echo "<br>";
+//echo $product->sport;
