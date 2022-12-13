90b477ccdf466cca26839947cd9405ec50002c6a
Nurkholis <m_nurkholis@icloud.com>
2022-12-13 13:35:10 +0700

    Trait

--- Trait/26.multiple_trait.php
+++ Trait/26.multiple_trait.php
@@ -1,25 +1,25 @@
 <?php
-trait SmartElectronic {
-    public function CheckOS()
-    {
-        return "Android 9.0 (Pie)";
-    }
-}
-
-trait LowWat {
-    public function efesiensi()
-    {
-        return "konsumsi daya 0.8";
-    }
-}
-
-class SmartTV {
-    use SmartElectronic, LowWat;
-    public function CheckInfo()
-    {
-        return "Smart TV ". $this->CheckOS(). "-" . $this->efesiensi();
-    }
-}
-
-$product = new SmartTV();
-echo $product->CheckInfo();
+//trait SmartElectronic {
+//    public function CheckOS()
+//    {
+//        return "Android 9.0 (Pie)";
+//    }
+//}
+//
+//trait LowWat {
+//    public function efesiensi()
+//    {
+//        return "konsumsi daya 0.8";
+//    }
+//}
+//
+//class SmartTV {
+//    use SmartElectronic, LowWat;
+//    public function CheckInfo()
+//    {
+//        return "Smart TV ". $this->CheckOS(). "-" . $this->efesiensi();
+//    }
+//}
+//
+//$product = new SmartTV();
+//echo $product->CheckInfo();
---
+++ Trait/27.trait_priority_1.php
@@ -0,0 +1,21 @@
+<?php
+//class Television {
+//    public function efesiensi()
+//    {
+//        return "Konsumsi daya 1.0";
+//    }
+//}
+//
+//trait LowWat {
+//    public function efesiensi()
+//    {
+//        return "Konsumsi daya 0.8";
+//    }
+//}
+//
+//class SmartTV extends Television {
+//    use LowWat;
+//}
+//
+//$product = new SmartTV();
+//echo $product->efesiensi();
