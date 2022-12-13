4d562bb593fc0e40ed2c5716440538d7a7e5a39b
Nurkholis <m_nurkholis@icloud.com>
2022-12-13 15:33:49 +0700

    namespace

---
+++ Magic Constan/45.magic_method_get.php
@@ -0,0 +1,13 @@
+<?php
+class Product {
+    public function __get($name) {
+        return "berikut adalah function dari $name";
+    }
+}
+
+$product = new Product();
+echo $product->masalah;
+echo "<br>";
+echo $product->testinfo;
+echo "<br>";
+echo $product->testlagi;
