f4cd27b5a37085c98cfefdf5c2edc908e792e5d6
Nurkholis <m_nurkholis@icloud.com>
2022-12-13 15:20:52 +0700

    Trait

---
+++ Magic Constan/41.magic_constant_class.php
@@ -0,0 +1,25 @@
+<?php
+//trait HardCover {
+//    public function checkTrait()
+//    {
+//        $msg = "Pesan ini dari method " . __METHOD__;
+//        $msg .= " di dalam trait " . __TRAIT__;
+//        return $msg;
+//    }
+//}
+//
+//class Book {
+//    use HardCover;
+//    public function checkClass()
+//    {
+//        $msg = "Pesan ini dari method " . __METHOD__;
+//        $msg .= " di dalam trait " . __TRAIT__;
+//        return $msg;
+//    }
+//}
+//
+//$product = new Book();
+//
+//echo $product->checkTrait();
+//echo "<br/>";
+//echo $product->checkClass();
