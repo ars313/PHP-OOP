d4b24b720456a989755c7af96e5183d769957bb5
Nurkholis <m_nurkholis@icloud.com>
2022-12-13 15:27:56 +0700

    namespace

---
+++ Magic Constan/42.magic_constan_namespace.php
@@ -0,0 +1,14 @@
+<?php
+//namespace Kholis;
+//
+//class Book {
+//    public function checkClass()
+//    {
+//        $msg = "Pesan ini dari class " . __CLASS__;
+//        $msg .= " Pesan ini berasala dari namespace " . __NAMESPACE__;
+//        return $msg;
+//    }
+//}
+//
+//$product = new Book();
+//echo $product->checkClass();
