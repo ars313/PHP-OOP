49ef9631e0df81104f009c632d6196471a3bfd64
Nurkholis <m_nurkholis@icloud.com>
2022-12-14 09:18:46 +0700

    perbandingan

---
+++ Perbandingan Object/75.perbandingan_object.php
@@ -0,0 +1,22 @@
+<?php
+//class Laptop {
+//    private $brand;
+//
+//    public function __construct($brand)
+//    {
+//        return $this->brand = $brand;
+//    }
+//}
+//
+//$product = new Laptop("Apple");
+//$product1 = new Laptop("Apple");
+//
+//if ($product1 == $product1)
+//    {
+//        echo "This product is same as Apple";
+//    }
+//
+//else {
+//    echo "This product is different from Apple";
+//}
+//
