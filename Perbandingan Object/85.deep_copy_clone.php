8323f09e114eb561b579e6d1c7517d7b8c943f48
Nurkholis <m_nurkholis@icloud.com>
2022-12-14 12:39:04 +0700

    deep copy

---
+++ Perbandingan Object/85.deep_copy_clone.php
@@ -0,0 +1,37 @@
+<?php
+//class CompanyName {
+//    public $name;
+//
+//    public function __construct($name) {
+//        return $this->name = $name;
+//    }
+//}
+//
+//class Laptop {
+//    public $brand;
+//    public $supplier;
+//
+//    public function __construct($brand)
+//    {
+//        return $this->brand = $brand;
+//    }
+//
+//    public function __clone() {
+//        $this->supplier = clone $this->supplier;
+//    }
+//}
+//
+//$suplierLaptop = new CompanyName("PT. Jaya Abadi");
+//$product = new Laptop("Dell");
+//$product->supplier = $suplierLaptop;
+//
+//$product2 = clone $product;
+//$product2->brand = "Apple";
+//$product2->supplier->name = "Apple.Inc";
+//
+//echo "<pre>";
+//print_r($product);
+//echo "</pre>";
+//echo "<pre>";
+//print_r($product2);
+//echo "</pre>";
