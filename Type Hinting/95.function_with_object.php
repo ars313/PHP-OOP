c4b68708f1a400f32707334e0a2fa18dee7ab8a0
Nurkholis <m_nurkholis@icloud.com>
2022-12-14 13:35:44 +0700

    method chaining

---
+++ Type Hinting/95.function_with_object.php
@@ -0,0 +1,21 @@
+<?php
+//class SmartPhone {
+//    public $brand;
+//    public $type;
+//    public $price;
+//
+//    public function __construct($brand, $type, $price) {
+//        $this->brand = $brand;
+//        $this->type = $type;
+//        $this->price = $price;
+//    }
+//}
+//
+//
+//function viewSmartPhone($hp){
+//    return "this brand smart phone : " . $hp->brand . "Type: " . $hp->type . "Price: ".number_format($hp->price
+//        ,2, ",",".");
+//}
+//
+//$product = new SmartPhone("Apple", "Iphone Xtends", 14000000);
+//echo viewSmartPhone($product);
