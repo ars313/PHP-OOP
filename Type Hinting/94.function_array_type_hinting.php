d82fc5fdfcb58240058578bcebf7f82f4d64c6c0
Nurkholis <m_nurkholis@icloud.com>
2022-12-14 13:28:09 +0700

    method chaining

---
+++ Type Hinting/94.function_array_type_hinting.php
@@ -0,0 +1,10 @@
+<?php
+//function hitungRataRata(array $data) {
+//    return ($data[0] + $data[1] + $data[2])/3;
+//}
+//
+//function printString(float $string) {
+//    return $string;
+//}
+//
+////echo hitungRataRata([3, 4, 14]);
