b527abc5d7b42fc88527aed347987f8f465c30ef
Nurkholis <m_nurkholis@icloud.com>
2022-12-14 13:18:35 +0700

    method chaining

--- Chaining/90.method_chaining.php
+++ Chaining/90.method_chaining.php
@@ -1,31 +1,31 @@
 <?php
-class Televisi {
-    private $brand;
-    private $typeScreen;
-    private $sizeScreen;
-
-    public function setBrand($brand) {
-        $this->brand = $brand;
-        return $this;
-    }
-
-    public function setTypeScreen($typeScreen) {
-        $this->typeScreen = $typeScreen;
-        return $this;
-    }
-
-    public function setSizeScreen($sizeScreen) {
-        $this->sizeScreen = $sizeScreen;
-        return $this;
-    }
-
-    public function infoTelevision() {
-        return "Televisi ". $this->brand . "With ". $this->typeScreen . "and". $this->sizeScreen . " inch";
-    }
-}
-
-$product = new Televisi();
-echo $product->setBrand("Samsung")
-    ->setSizeScreen(42)
-    ->setTypeScreen("LCD")
-    ->infoTelevision();
+//class Televisi {
+//    private $brand;
+//    private $typeScreen;
+//    private $sizeScreen;
+//
+//    public function setBrand($brand) {
+//        $this->brand = $brand;
+//        return $this;
+//    }
+//
+//    public function setTypeScreen($typeScreen) {
+//        $this->typeScreen = $typeScreen;
+//        return $this;
+//    }
+//
+//    public function setSizeScreen($sizeScreen) {
+//        $this->sizeScreen = $sizeScreen;
+//        return $this;
+//    }
+//
+//    public function infoTelevision() {
+//        return "Televisi ". $this->brand . "With ". $this->typeScreen . "and". $this->sizeScreen . " inch";
+//    }
+//}
+//
+//$product = new Televisi();
+//echo $product->setBrand("Samsung")
+//    ->setSizeScreen(42)
+//    ->setTypeScreen("LCD")
+//    ->infoTelevision();
