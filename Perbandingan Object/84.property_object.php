53403323f8bdb7635c55aa70a1e085b2e504224b
Nurkholis <m_nurkholis@icloud.com>
2022-12-14 12:28:35 +0700

    shallow copy

---
+++ Perbandingan Object/84.property_object.php
@@ -0,0 +1,25 @@
+<?php
+//class Company {
+//    public $name;
+//
+//    public function __construct($name) {
+//        $this->name = $name;
+//    }
+//}
+//
+//class Laptop {
+//    public $brand;
+//    public $suplier;
+//
+//    public function __construct($brand) {
+//        $this->brand = $brand;
+//    }
+//}
+//
+//$product = new Company("CV. Abadi semesta");
+//$product1 = new Laptop("Apple");
+//$product1->suplier = $product;
+//
+//echo "<pre>";
+//print_r($product1);
+//echo "</pre>";
